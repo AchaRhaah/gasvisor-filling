7e6c45fe8df4ac9d398709f47120c7d4bfe65add
AchaRhaah <acharhaah2003@gmail.com>
2023-02-14 18:16:08 +0100

    created a donut chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJs, Tooltip, Title, ArcElement, Legend } from "chart.js";
+
+ChartJs.register(Tooltip, Title, ArcElement, Legend);
 
 function App() {
+  const [data, setData] = useState({
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: ["red", "blue", "yellow"],
+      },
+    ],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: ["Red", "Blue", "Yellow"],
+  });
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((data) => {
+          const res = data.json();
+          return res;
+          // console.log("ress: ", res);
+        })
+        .then((res) => {
+          console.log("res: ", res);
+          const label = [];
+          const data = [];
+          for (var i of res) {
+            label.push(i.name);
+            data.push(i.id);
+          }
+          setData({
+            datasets: [
+              {
+                data: data,
+                backgroundColor: [
+                  "red",
+                  "blue",
+                  "yellow",
+                  "purple",
+                  "orange",
+                  "indigo",
+                  "violet",
+                  "green",
+                  "pink",
+                  "gray",
+                ],
+              },
+            ],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: label,
+          });
+        })
+        .catch((e) => {
+          console.log("error: ", e);
+        });
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      {/* <h1>Donut chart</h1> */}
+      <Doughnut data={data} style={{ width: "20em", height: "20em" }} />
     </div>
   );
 }
