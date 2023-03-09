235aa12d2663cab448b6c235aba32bee42469653
AchaRhaah <acharhaah2003@gmail.com>
2023-03-09 17:34:01 +0100

    made cosmetic fixes

--- src/components/Cylinder.js
+++ src/components/Cylinder.js
@@ -4,19 +4,22 @@ import "./styles.css";
 function Cylinder({ percentageRemaining }) {
   return (
     <div className="cylinder-container">
-      <img src={require("../images/top.png")} alt="" className="top" />
       <div className="cylinder">
-        <div
-          style={{
-            height: `${percentageRemaining - 10}%`,
-          }}
-          className="level"
-        >
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <p className="percent">{percentageRemaining}%</p>
+        <img src={require("../images/top.png")} alt="" className="top" />
+        <div className="cylinder-body">
+          <div
+            style={{
+              height: `${percentageRemaining - 10}%`,
+            }}
+            className="level"
+          >
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <p className="percent">{percentageRemaining}%</p>
+          </div>
         </div>
       </div>
+
       <h3 className="cylinder-caption">Real time daily usage</h3>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,18 @@ import Logo from "../images/logo.svg";
 function Header() {
   return (
     <div className="header">
-      <div className="logo">
-        <img src={Logo} alt="" className="gas-logo" />
-      </div>
-      <div className="right">
-        <p className="welcome">Welcome back, MartMich!</p>
-        <img className="profile" alt="" src={require("../images/david.png")} />
+      <div className="content">
+        <div className="logo">
+          <img src={Logo} alt="" className="gas-logo" />
+        </div>
+        <div className="right">
+          <p className="welcome">Welcome back, MartMich!</p>
+          <img
+            className="profile"
+            alt=""
+            src={require("../images/david.png")}
+          />
+        </div>
       </div>
     </div>
   );
