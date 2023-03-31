bf8d32c4ee696f7889fa7b630c996a16a84593fb
AchaRhaah <acharhaah2003@gmial.com>
2023-03-31 03:49:10 -0800

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import {
@@ -28,16 +28,15 @@ function App() {
   const backgroundArray = [];
   const [percent, setPercent] = useState(100);
   var currentWeight = [0, 0, 0, 0, 0, 0, 0];
-  var [maxYscaleVal, setMaxYScale] = useState(0)
+  var [maxYscaleVal, setMaxYScale] = useState(0);
   var cylinderWeight = [];
-  var [usedWeight, setUsedWeight] = useState([0,0,0,0,0,0,0])
-  var percentArr = ['none','none','none','none','none','none','none']
+  var [usedWeight, setUsedWeight] = useState([0, 0, 0, 0, 0, 0, 0]);
+  var percentArr = ["none", "none", "none", "none", "none", "none", "none"];
   var [apiData, setApiData] = useState([]);
-  var [loading, setLoding] = useState(false)
-  var [fetchedDate, setFetchedDate] = useState("")
-  
+  var [loading, setLoding] = useState(false);
+  var [fetchedDate, setFetchedDate] = useState("");
 
-    const [data, setData] = useState({
+  const [data, setData] = useState({
     labels: ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"],
     datasets: [
       {
@@ -49,8 +48,7 @@ function App() {
       },
     ],
   });
-  
-  
+
   useEffect(() => {
     fetch("https://api.gasvisor.eu/api/sensors/data/gasvisor_sensor_02", {
       method: "get",
@@ -60,64 +58,103 @@ function App() {
     })
       .then((data) => data.json())
       .then((data) => {
-        setApiData(data)
+        setApiData(data);
         currentWeight = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
         // To get the currnet day's date in words format
-        var date = new Date(data[data.length - 1].date_created)
-        const dt = date.getDay()
-        var dateArr = date.toDateString().split(' ');
-        var dateFormat = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3];
-        setFetchedDate(dateFormat)
-        setLoding(true)
+        var date = new Date(data[data.length - 1].date_created);
+        const dt = date.getDay();
+        var dateArr = date.toDateString().split(" ");
+        var dateFormat = dateArr[2] + " " + dateArr[1] + " " + dateArr[3];
+        setFetchedDate(dateFormat);
+        setLoding(true);
         setPercent(Math.trunc(data[data.length - 1].percentage_weight));
 
-// variables needed to separate api data into days
-        
+        // variables needed to separate api data into days
+
         var arrFirstDate = new Date(data[0].date_created).getDate();
         var arrDayIndex = new Date(data[0].date_created).getDay();
+        console.log("**arrDayIndex", data[0].date_created);
         data.map((item, index) => {
-          cylinderWeight.push(parseInt(item.cylinder_weight))
+          cylinderWeight.push(parseInt(item.cylinder_weight));
 
-// if date of index is different from the date of the previous object then it is a new day
-          
+          // if date of index is different from the date of the previous object then it is a new day
+
           if (new Date(data[index].date_created).getDate() != arrFirstDate) {
-            currentWeight.splice(arrDayIndex - 1, 1, Math.trunc(item.calculated_weight))
-            usedWeight.splice(arrDayIndex - 1, 1, item.weight_used)
-            arrFirstDate = new Date(data[index].date_created).getDate()
-            arrDayIndex = new Date(data[index].date_created).getDay()
+            console.log(
+              " info: dare ,day",
+              arrFirstDate,
+              arrDayIndex,
+              parseFloat(data[index - 1].calculated_weight).toFixed(2)
+            );
+            currentWeight.splice(
+              arrDayIndex - 1,
+              1,
+              parseFloat(data[index - 1].calculated_weight).toFixed(2)
+            );
+
+            usedWeight.splice(arrDayIndex - 1, 1, data[index - 1].weight_used);
+            percentArr.splice(
+              arrDayIndex - 1,
+              1,
+              parseFloat(data[index - 1].percentage_weight).toFixed(2)
+            );
+
+            arrFirstDate = new Date(data[index].date_created).getDate();
+            arrDayIndex = new Date(data[index].date_created).getDay();
           }
 
-// The last object in the array is the most current reading for that day
+          // The last object in the array is the most current reading for that day
 
           if (index === data.length - 1) {
-            currentWeight.splice(arrDayIndex - 1, 1, parseFloat(item.calculated_weight).toFixed(2));
-            usedWeight.splice(arrDayIndex - 1, 1, parseFloat(item.weight_used).toFixed(2));
-            percentArr.splice(arrDayIndex - 1, 1, parseFloat(item.percentage_weight).toFixed(2));
+            currentWeight.splice(
+              arrDayIndex - 1,
+              1,
+              parseFloat(item.calculated_weight).toFixed(2)
+            );
+            usedWeight.splice(
+              arrDayIndex - 1,
+              1,
+              parseFloat(item.weight_used).toFixed(2)
+            );
+            percentArr.splice(
+              arrDayIndex - 1,
+              1,
+              parseFloat(item.percentage_weight).toFixed(2)
+            );
           }
-        })
-        setUsedWeight(usedWeight)
+        });
+        setUsedWeight(usedWeight);
         const background = () => {
           percentArr.map((num, index) => {
-            num === 'none' ? backgroundArray.push("#c9c9da") :  num <= 15 && index <= dt - 1? backgroundArray.push("#E64646") : index < dt-1 ? backgroundArray.push("#2A297D") : index === dt-1 ? backgroundArray.push("#FDBD2B") : backgroundArray.push("#c9c9da")
-          })
+            num === "none"
+              ? backgroundArray.push("#c9c9da")
+              : num <= 15 && index <= dt - 1
+              ? backgroundArray.push("#E64646")
+              : index < dt - 1
+              ? backgroundArray.push("#2A297D")
+              : index === dt - 1
+              ? backgroundArray.push("#FDBD2B")
+              : backgroundArray.push("#c9c9da");
+          });
         };
-        
-    
-        background(); 
-        maxYscaleVal = data[data.length - 1].cylinder_weight_full - data[data.length - 1].cylinder_weight_empty
-        setMaxYScale(maxYscaleVal)
+
+        background();
+        maxYscaleVal =
+          data[data.length - 1].cylinder_weight_full -
+          data[data.length - 1].cylinder_weight_empty;
+        setMaxYScale(maxYscaleVal);
         setData({
-    labels: ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"],
-    datasets: [
-      {
-        data: currentWeight,
-        backgroundColor: backgroundArray,
-        barThickness: 30,
-        borderRadius: 20,
-        borderSkipped: false,
-      },
-    ],
-        })
+          labels: ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"],
+          datasets: [
+            {
+              data: currentWeight,
+              backgroundColor: backgroundArray,
+              barThickness: 30,
+              borderRadius: 20,
+              borderSkipped: false,
+            },
+          ],
+        });
       })
       .catch((err) => console.log(err));
   }, []);
@@ -131,17 +168,34 @@ function App() {
           <Cylinder percentageRemaining={percent} />
         </di>
         <di>
-
-          <Barchart chartData={data} usedWeight={usedWeight} maxYscaleVal={ maxYscaleVal} />
+          <Barchart
+            chartData={data}
+            usedWeight={usedWeight}
+            maxYscaleVal={maxYscaleVal}
+          />
         </di>
       </div>
-     {loading ?  <div className="info-box">
-        <h3 className="heading heading2">Real time details for {fetchedDate}</h3>
-        <p className="info">Weight of full cylinder: {maxYscaleVal} kg</p>
-        <p className="info">Current weight of gas: {parseFloat(apiData[apiData.length - 1].calculated_weight).toFixed(2)} kg</p>
-        <p className="info">Weight used: {parseFloat(apiData[apiData.length - 1].weight_used).toFixed(2)} kg</p>
-      </div> : <></>}
-     
+      {loading ? (
+        <div className="info-box">
+          <h3 className="heading heading2">
+            Real time details for {fetchedDate}
+          </h3>
+          <p className="info">Weight of full cylinder: {maxYscaleVal} kg</p>
+          <p className="info">
+            Current weight of gas:{" "}
+            {parseFloat(apiData[apiData.length - 1].calculated_weight).toFixed(
+              2
+            )}{" "}
+            kg
+          </p>
+          <p className="info">
+            Weight used:{" "}
+            {parseFloat(apiData[apiData.length - 1].weight_used).toFixed(2)} kg
+          </p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
